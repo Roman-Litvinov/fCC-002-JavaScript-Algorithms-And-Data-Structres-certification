35fc9c291af9872dd9b6b401d800599593e19e96
Roman Litvinov <katman@gmail.com>
2021-08-01 10:33:12 +1000

    Basic JS1 - some updates to recursion and object lessons ES6 continued

--- 001 - Basic Javascript/101-replace-loops-using-recursion.js
+++ 001 - Basic Javascript/101-replace-loops-using-recursion.js
@@ -1,54 +1,51 @@
-/*  RECURSION, also know as sexy magic
-    - means a function can be expressed in terms of itself
-    - a recursive function contains a call to itself in its code
-
-A recursive function should have a BASE CASE in its code that returns
-something without calling the function again - otherwise the function
-will never stop executing. Usually the actual calculation begins from
-the base case.
-*/
-
-// EXAMPLE
-// write a function using loops that takes the first 
-// N elements of an array and returns their product:
-function multiply(myArray, n) {
-  var product = 1;
-  for (var i = 0; i < n; i++) {
-    product *= myArray[i];
-  }
-  return product;
+// muliply first N elements of the array - regular function:
+function multiply(arr, n)
+{
+	let product = 1;
+	for (let i = 0; i < n; i++)
+	{
+		product *= arr[i];
+	}
+	return product;
 }
-console.log(multiply([1,2,3,4,5], 4));
+let myArray = [15, 20, 3, 2.5, 5];
+let n = 100;
+console.log("Product of the first " + n + " elements of the array", myArray, "is: " + multiply(myArray, n) + ".");
 
-
-// now make the above into a recursive function
-// that calls itself and doesn't use any loops:
-function multiplyRecursive(myArray, n) {
-
+// multiply first N elements of the array - recursive function
+// (now checking for N being 0 or larger than array's length!):
+function multiplyRecursive(arr, n)
+{
+	if (n <= 0)
+	{					// check for first 0 elements passed as an argument -> return undefined (included so as NOT to return 1!)
+		return undefined;
+	} else if (n > arr.length)
+	{	// check if number of elements requested is larger than length of the array -> return all available elements
+		return multiplyRecursive(arr, arr.length);
+	} else if (n == 1)
+	{			// base case - first 1 element -> return element 0
+		return arr[n - 1];
+	} else
+	{						// otherwise, recursion
+		return arr[n - 1] * multiplyRecursive(arr, n - 1);
+	}
 }
+console.log("Product of the first " + n + " elements of the array", myArray, "is: " + multiplyRecursive(myArray, n) + ".");
 
 
-
-// write a recursive function that returns the sum
-// of the first N elements of the array ARR:
-function sum(arr, n) {
-  // BASE CASE - reached when we call sum(arr, 0) from sum(arr, 1), so we return 0,
-  // then we start going back up: to sum(arr, 1), returning 0 + arr[0],
-  // and to sum(arr, 2), returning 0 + arr[0] + arr[1],
-  // and to sum(arr, 3), returning 0 + arr[0] + arr[1] + arr[2],
-  // and so on, until we reach N and the return value is: 0 + arr[0] + arr[1] + arr[2] + ... + arr[N-1],
-  // which is the sum of the first N elements of the array - that have indices from 0 to N-1:
-  if (n == 0) {
-    return 0;
-  } else {
-    return sum(arr, n-1) + arr[n-1];
-  }
+// recursive function to return a sum of the first N elements of a given array
+// (no edge cases checking implemented):
+function sum(arr, n)
+{
+	if (n <= 0)
+	{
+		return 0;
+	} else
+	{
+		return arr[n - 1] + sum(arr, n - 1);
+	}
 }
-
-// TEST CASES:
-// should return 0:
-console.log('\n'+sum([1], 0));
-// should return 2:
-console.log(sum([2,3,4], 1));
-// should return 9:
-console.log(sum([2,3,4,5], 3));
+// test cases:
+console.log(sum([1], 0));
+console.log(sum([2, 3, 4], 1));
+console.log(sum([2, 3, 4, 5], 3));
--- 001 - Basic Javascript/110-use-recursion-to-create-a-countdown.js
+++ 001 - Basic Javascript/110-use-recursion-to-create-a-countdown.js
@@ -1,74 +1,34 @@
-// EXAMPLE
-// define a recursive function to create a count-up:
-// - take a number as an argument,
-// - return an array of consecutive integers starting
-//   with 1 through the received number
-//
-// This recursive function will have:
-// - a base case, where the function returns a specific value
-//   (maybe f(0), returning an empty array?)
-// - a recursion call to itself with a decremented parameter
-//
-// Recursive logic:
-// f(N) =   [1,2,..,N-2,N-1,N]
-// f(N-1) = [1,2,..,N-2,N-1]
-// f(N-2) = [1,2,..,N-2]
-// ...
-// f(2) =   [1,2]
-// f(1) =   [1]
-// f(0) =   []  <-- base case?
-
-function countUp(n) {
-  // set up the base case - when n is 0:
-  if (n < 1) {
-    // return an empty array (thus initializing the final array):
-    return [];
-  } else {
-    // init the final array by assigning it the result of the recursive
-    // function call to itself with the next smaller argument:
-    var upArray = countUp(n-1);
-    // append the current value to the resulting array - which has been
-    // returned from the recursive call to itself with the previous smaller
-    // argument:
-    upArray.push(n);
-    // return the updated final array:
-    return upArray;
-  }
-} 
-console.log(countUp(7));
-
-
-// return an array of integers, counting
-// down from the given number N to 1:
-function countdown(n) {
-  if (n < 1) {
-    // base case - return an empty array and go back up the recursions:
-    return [];      
-  } else
-    // recurse down with a smaller argument:
-    var downArray = countdown(n-1);
-    // add current argument to the front of the array:
-    downArray.unshift(n);
-    // return function result - either up the recursions or final result:
-    return downArray;
+// function that returns an array of numbers from start to N, recursively:
+function countUp(start, n)
+{
+	if (n < start)
+	{
+		return [];		// this is the base case - this is where we initialize an empty array to start filling
+	} else
+	{
+		let myArr = countUp(start, n - 1);	// recursive call to itself with a previous N - base case makes myArr an empty array after this
+		myArr.push(n);						// appending a current N as an element to the end of the array
+		return myArr;						// returning the array - either recursing up, or final output
+	}
 }
+console.log(countUp(-3, 5));
 
-// TEST CASES:
-// should return an empty array:
+// function that returns an array of numbers from N to 1, recursively:
+function countdown(n)
+{
+	if (n < 1)
+	{
+		return [];		// base case - empty array is created here, with N being 0
+	} else
+	{
+		let myArr = countdown(n - 1);		// recursive call to itself with a previous N - base case makes myArr an empty array after this
+		myArr.unshift(n);				// add a current N as an element to  the beginning of the array (increasing the number as we recurse up)
+		return myArr;					// returning the array - either recursing up, or final output
+	}
+
+}
+// test cases
 console.log(countdown(-1));
-// should return [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]:
 console.log(countdown(10));
-// should return [5, 4, 3, 2, 1]:
 console.log(countdown(5));
-// should return an empty array:
-console.log(countdown(0));
-
-
-
-
-// A LITTLE SIDE EXPERIMENT - use CONCAT() method of arrays
-// to merge an array to an array, without changing the order
-// of elements.
-// CONCAT() method returns an array as a result:
-var myArray = [1,2,3].concat([1,2,5]);
-// console.log(myArray);
+console.log(countdown(-7));
